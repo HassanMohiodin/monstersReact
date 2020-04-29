6fc75771c2fcbd0a3387d8dcf61812d0b5d0ba24
HassanMohiodin <55244353+HassanMohiodin@users.noreply.github.com>
2020-04-30 04:35:41 +0500

    Updates

---
+++ precache-manifest.9b7b505ab6c1156d8ac83374ca6dcbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6164b91ebc419763364afa82f1d1e37",
+    "url": "/monstersReact/index.html"
+  },
+  {
+    "revision": "309939558d91180b95f9",
+    "url": "/monstersReact/static/css/main.9b0d5630.chunk.css"
+  },
+  {
+    "revision": "a6891c50111b42bfc899",
+    "url": "/monstersReact/static/js/2.35e20edb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersReact/static/js/2.35e20edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309939558d91180b95f9",
+    "url": "/monstersReact/static/js/main.9661c42f.chunk.js"
+  },
+  {
+    "revision": "ea75ca5c8a19d21d6a43",
+    "url": "/monstersReact/static/js/runtime-main.657903c0.js"
+  }
+]);
